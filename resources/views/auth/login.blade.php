4a0950dbbdc44b692d2c8fbf2b5f88e7004c36f2
kirukata33 <ivalbiasaaja@gmail.com>
2026-08-20 13:05:55 +0700

    commit ajea

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,7 +20,7 @@
                     <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/>
                 </svg>
             </div>
-            <span class="brand-name">Laravel × Odoo 19</span>
+            <span class="brand-name">Inventori</span>
         </a>
         <span class="top-bar-info">Sistem Manajemen Terintegrasi</span>
     </header>
